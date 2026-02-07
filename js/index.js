9569a2d7d950afc19726853147acd38a87b83b18
坂口　晃平 <2250399@ecc.ac.jp>
2026-02-07 18:57:31 +0900

    ファイル整理

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 const inputNum = document.getElementById("loginNum");
 const savaBtn = document.getElementById("savaBtn");
 
+// inputNum.addEventListener("input", () => {
+//     inputNum.value = inputNum.value.replace(/\D/g, "")
+// });
 inputNum.addEventListener("input", () => {
-    inputNum.value = inputNum.value.replace(/\D/g, "")
+    inputNum.value = inputNum.value.replace(/[0-9]/g, s => String.fromCharCode(s.charCodeAt(0) - 65248))
 });
 
 savaBtn.addEventListener("click", () => {
