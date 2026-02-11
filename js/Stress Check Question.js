38ab55813f13b391fc427ee40ea1d6b3f55cdc0c
坂口　晃平 <2250399@ecc.ac.jp>
2026-02-12 03:53:04 +0900

    毎日アンケート完成 管理者画面作成

--- js/Stress Check Question.js
+++ js/Stress Check Question.js
@@ -1,22 +1,35 @@
 const innerWrap = document.querySelector(".innerWrap");
+const main = document.querySelector(".main");
+const backBtn = document.getElementById("back");
+const slider = document.getElementById("slider");
 const contents = document.getElementsByClassName("contents");
 const slides = document.querySelectorAll(".question").length;
 const nextBtn = document.querySelectorAll(".button")
+const nextPageBtn = document.getElementById("nextPageBtn");
+const compWrap = document.querySelector(".compWrap");
 let current = 0;
 
 console.log(contents);
-
+console.log(nextPageBtn);
 
 nextBtn.forEach((btn, i) => {
     btn.addEventListener("click", next)
 });
 
 function next() {
-    current++
-    // innerWrap.style
-    innerWrap.classList.add(`pos${current}`);
+    if (current < contents.length) {
+        current++
+        slider.classList.add(`pos${current}`);
+    }
+};
+
+backBtn.addEventListener("click", () => {
+    current - 1;
+    slider.classList.remove(`pos${current}`);
     console.log(current);
+});
 
-}
-
-
+nextPageBtn.addEventListener("click", () => {
+    main.classList.add("none");
+    compWrap.classList.remove("none")
+});
